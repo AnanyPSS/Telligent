1319c20c3c1830f568e86f1c93b52d21d240c128
Ananya Pratap <ananyapratap28@gmail.com>
2020-03-16 14:16:12 +0530

    login view updation and making it functional

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_110712) do
+ActiveRecord::Schema.define(version: 2020_03_16_075925) do
 
   create_table "articles", force: :cascade do |t|
     t.text "article"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,8 +32,11 @@ ActiveRecord::Schema.define(version: 2020_03_15_110712) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
